b619e53eeb04614d26d9d6d02de94bea64dcbd85
swu1337 <master_msp1@hotmail.com>
2018-10-02 11:34:56 +0200

    optimized fonts choices and css linking javascript onload removed moved script tag to bottom

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,17 +1,15 @@
-window.onload = function() {
-    let menu = document.querySelector('.menu');
-    let nav = document.querySelector('nav');
-    let icn = document.querySelector('.menu-icn > i');
-    
-    menu.addEventListener('click', function(){
-        if(nav.classList.contains('open')) {
-            nav.classList.remove('open');
-            icn.classList.remove('fa-times');
-            icn.classList.add('fa-bars');
-        } else {
-            nav.classList.add('open');
-            icn.classList.remove('fa-bars');
-            icn.classList.add('fa-times');
-        }
-    });    
-}
+let menu = document.querySelector('.menu');
+let nav = document.querySelector('nav');
+let icn = document.querySelector('.menu-icn > i');
+
+menu.addEventListener('click', function(){
+    if(nav.classList.contains('open')) {
+        nav.classList.remove('open');
+        icn.classList.remove('fa-times');
+        icn.classList.add('fa-bars');
+    } else {
+        nav.classList.add('open');
+        icn.classList.remove('fa-bars');
+        icn.classList.add('fa-times');
+    }
+});    
